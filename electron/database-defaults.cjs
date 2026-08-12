a8cb9b04d7bca2b75c6e2c8aaa84051b1b41ecd8
luoringzl <415663776@qq.com>
2026-08-12 20:23:33 +0800

    feat: add Electron V7 database defaults

---
+++ electron/database-defaults.cjs
@@ -0,0 +1,31 @@
+const DB_VERSION = 7;
+
+const DEFAULT_PLANNING_SETTINGS = Object.freeze({
+  forecastHorizons:[30,60,90],
+  defaultForecastDays:30,
+  defaultGanttDays:60,
+  defaultGanttViewportDays:21,
+  recommendationDays:30,
+  autoScheduleDays:60,
+  maxPeoplePerNeed:4,
+  maxAllocationChunk:50,
+  minAllocationChunk:10,
+  allocationStep:10,
+  workingDays:[1,2,3,4,5],
+  capacityUnit:'percent'
+});
+
+function emptyDatabase() {
+  return {
+    version: DB_VERSION,
+    meta: { schemaVersion:DB_VERSION, migrations:[] },
+    updatedAt: new Date().toISOString(),
+    projects: [], people: [], assignments: [], staffingNeeds: [], activity: [],
+    settings: {
+      companyName:'', warningDays:7, dictionaries:{}, customFields:{projects:[],people:[]},
+      planning:{...DEFAULT_PLANNING_SETTINGS,forecastHorizons:[...DEFAULT_PLANNING_SETTINGS.forecastHorizons],workingDays:[...DEFAULT_PLANNING_SETTINGS.workingDays]}
+    }
+  };
+}
+
+module.exports={DB_VERSION,DEFAULT_PLANNING_SETTINGS,emptyDatabase};
